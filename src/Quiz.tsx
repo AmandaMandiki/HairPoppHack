d75e67944e28c9e1953959d1106505e7ee6f9237
Amanda-Mandiki <amanda.mandiki@hotmail.co.uk>
2023-07-22 13:51:37 +0100

    Adding folders

---
+++ src/Quiz.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HelloPage from "./Quiz/HelloPage/HelloPage.tsx";
+
+export default function Quiz() {
+  return (
+    <div className="float-left">
+      Quiz Page
+      <HelloPage />
+      
+    </div>
+  );
+}
