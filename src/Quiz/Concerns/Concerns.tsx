5a4c921447269c4b4a162e73ccd99dbcf9d00d2e
Amanda-Mandiki <amanda.mandiki@hotmail.co.uk>
2023-07-22 16:14:03 +0100

    New pages

--- src/Quiz/Concerns/Concerns.tsx
+++ src/Quiz/Concerns/Concerns.tsx
@@ -2,23 +2,54 @@ import React from "react";
 
 export default function Concerns() {
   const concernsType = [
-    { type: "Fine weak hair", value: null, detail: ''},
-    { type: "Oily / Greasy Hair", value: null },
-    { type: "Dandruff", value: null },
-    { type: "Sensitive scalp", value: null },
-    { type: "Damaged hair", value: null },
-    { type: "Dry", value: null },
+    {
+      type: "Fine weak hair",
+      value: null,
+      detail: "Thin, fragile hair prone to breakage.",
+    },
+    {
+      type: "Oily / Greasy Hair",
+      value: null,
+      detail: " Excess sebum production makes hair look greasy.",
+    },
+    {
+      type: "Dandruff",
+      value: null,
+      detail: "Scalp condition with white/yellowish flakes and itching.",
+    },
+    {
+      type: "Sensitive scalp",
+      value: null,
+      detail: "Prone to irritation from hair products or weather conditions.",
+    },
+    {
+      type: "Damaged hair",
+      value: null,
+      detail: " Weakened, frizzy hair due to heat, chemicals, or environment.",
+    },
+    {
+      type: "Dry",
+      value: null,
+      detail: "Lacks moisture, appears dull and frizzy.",
+    },
   ];
 
   return (
     <div>
-      Concerns
-      {concernsType.map(({ type, value }) => (
-        <div>
-          <input type="checkbox" id={type} name={type} value={value} />
-          <label for={type}>{type}</label>
-        </div>
-      ))}
+      <span className="text-4xl"> Select your hair concerns </span>
+      <div className="mx-40 flex flex-wrap justify-center">
+        {concernsType.map(({ type, value, detail }) => (
+          <div className="m-4 py-10 bg-gray-100">
+            <input type="checkbox" id={type} name={type} value={value} />
+            <label className="ml-4" for={type}>
+              {type}
+            </label>
+            <div className="border border-gray-500 rounded bg-purple-500">
+              {detail}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Quiz/Goals/Goals.tsx
+++ src/Quiz/Goals/Goals.tsx
@@ -1,5 +1,53 @@
 import React from "react";
 
 export default function Goals() {
-  return <div>Goals</div>;
+   const Goals = [
+     {
+       type: "Volume",
+       value: null,
+       detail: "Thin, fragile hair prone to breakage.",
+     },
+     {
+       type: "Deep condition",
+       value: null,
+       detail: " Excess sebum production makes hair look greasy.",
+     },
+     {
+       type: "Anti frizz",
+       value: null,
+       detail: "Scalp condition with white/yellowish flakes and itching.",
+     },
+     {
+       type: "Oil control",
+       value: null,
+       detail: "Prone to irritation from hair products or weather conditions.",
+     },
+     {
+       type: "Nourish",
+       value: null,
+       detail: " Weakened, frizzy hair due to heat, chemicals, or environment.",
+     },
+     {
+       type: "Shine",
+       value: null,
+       detail: "Lacks moisture, appears dull and frizzy.",
+     },
+   ];
+  return (
+    <div>
+      <span className="text-4xl"> Select your hair goals </span>
+      <div className="mx-40 flex flex-wrap justify-center">
+        {Goals.map(({ type, value, detail }) => (
+          <button className="border border-gray-100 m-4 py-10 bg-gray-100 rounded-lg hover:bg-purple-200">
+            <label className="mx-4" for={type}>
+              {type}
+            </label>
+            <div className="hidden border border-gray-500 rounded bg-purple-500">
+              {detail}
+            </div>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
