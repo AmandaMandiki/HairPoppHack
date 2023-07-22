28cedb1395de13e100404e5c1b2d2c297a6fd0ca
Amanda-Mandiki <amanda.mandiki@hotmail.co.uk>
2023-07-22 14:27:18 +0100

    All routers

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import HelloPage from "./Quiz/HelloPage/HelloPage.tsx";
+import Allergy from "./Quiz/Allergy/Allergy.tsx";
+import Concerns from "./Quiz/Concerns/Concerns.tsx";
+import Goals from "./Quiz/Goals/Goals.tsx";
+import Age from "./Quiz/Age/Age.tsx"
+import HairType from "./Quiz/HairType/HairType.tsx";
+import Results from "./Quiz/Results/Results.tsx";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -9,6 +15,12 @@ export default function Quiz() {
       <BrowserRouter>
         <Routes>
           <Route path="/Hellopage" index element={<HelloPage />} />
+          <Route path="/Allergy" index element={<Allergy />} />
+          <Route path="/Concerns" index element={<Concerns />} />
+          <Route path="/Goals" index element={<Goals />} />
+          <Route path="/Age" index element={<Age />} />
+          <Route path="/HairType" index element={<HairType />} />
+          <Route path="/Results" index element={<Results />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Quiz/HairType/HairType.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HairType() {
+  return <div>Hair Type</div>;
+}
